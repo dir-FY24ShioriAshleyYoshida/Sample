d0ae0a7ea333e6e6f6a72a3be826dc115e0729c4
Shioriash <ashleyshiori.yoshida.y332@dirbato.co.jp>
2024-04-08 11:08:01 +0900

    else if文追加

--- Sample/src/sample/IfSample.java
+++ Sample/src/sample/IfSample.java
@@ -3,9 +3,13 @@ package sample;
 public class IfSample {
 
     public static void main(String[] args) {
-        var score=50;//成績
+        var score=100;//成績
         
-        if(score>=60) {
+        if(score==100) {
+            System.out.println("満点です!");
+        }else if(score >= 80) {
+            System.out.println("よくできました!");
+        }else if(score >=60) {
             System.out.println("合格です!");
         }else {
             System.out.println("赤点です。。。");
