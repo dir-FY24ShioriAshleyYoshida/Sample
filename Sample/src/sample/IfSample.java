611111131513544f6bca070a8024ae7d9f4252e5
Shioriash <ashleyshiori.yoshida.y332@dirbato.co.jp>
2024-04-08 11:02:29 +0900

    if-else文追加

--- Sample/src/sample/IfSample.java
+++ Sample/src/sample/IfSample.java
@@ -3,10 +3,12 @@ package sample;
 public class IfSample {
 
     public static void main(String[] args) {
-        var score=80;//成績
+        var score=50;//成績
         
         if(score>=60) {
             System.out.println("合格です!");
+        }else {
+            System.out.println("赤点です。。。");
         }
 
     }
